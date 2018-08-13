87a9dc68ed1feb4747370b625ce285b0b8fee14d
tortoiselala <tortoiselala@gmail.com>
2018-08-14 00:11:13 +0800

    control pane

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,20 +1,18 @@
 package gameOfLife;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
-import java.awt.TextArea;
 import java.awt.Toolkit;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextPane;
-import javax.tools.OptionChecker;
 
 public class ControlPanel extends JPanel {
 	/**
@@ -23,6 +21,9 @@ public class ControlPanel extends JPanel {
 	private static final long serialVersionUID = -7869229682601225660L;
 	private double screenWidth;
 	private double screenHeight;
+	private GridBagLayout layout;
+	final static int minbuttonWidth = 100;
+	final static int minbuttonHeight = 100;
 
 	public ControlPanel() {
 
@@ -31,7 +32,7 @@ public class ControlPanel extends JPanel {
 
 		setBackground(Color.RED);
 
-		setOpaque(false);
+		setOpaque(true);
 
 		setBounds((int) (getScreenWidth() - getScreenWidth() / 4), 0, (int) (getScreenWidth() / 4),
 				(int) (getScreenHeight() / 2));
@@ -44,52 +45,47 @@ public class ControlPanel extends JPanel {
 	}
 
 	public void addYAxisBox() {
-		GridBagLayout layout = new GridBagLayout();
+		layout = new GridBagLayout();
 		setLayout(layout);
-		
-		GridBagConstraints container = new GridBagConstraints();
-		
-		 
-		
-		JPanel initPanel = new JPanel();
 
-		JLabel initText = new JLabel("预置");
-		initText.setFont(new Font("宋体", Font.BOLD, 20));
+		JLabel initText = new JLabel("预置:");
+		initText.setFont(new Font("微软雅黑", Font.BOLD, 20));
 
 		JComboBox<String> initCheckBox = new JComboBox<String>();
-
+		initCheckBox.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		initCheckBox.addItem("Pulsar");
 		initCheckBox.addItem("Glider");
 		initCheckBox.addItem("Gospel Gliding Gun");
-		initCheckBox.setOpaque(true);
 
-		initPanel.add(initText);
-		initPanel.add(initCheckBox);
+		addComponent(0, 0, 2, 1, 0, 0, initText, GridBagConstraints.NONE, GridBagConstraints.EAST);
+		addComponent(2, 0, 6, 1, 0, 0, initCheckBox, GridBagConstraints.NONE, GridBagConstraints.CENTER);
+		addComponent(0, 1, 2, 1, 0, 0, createButton("start/pause"), GridBagConstraints.NONE,
+				GridBagConstraints.CENTER);
+		addComponent(2, 1, 2, 1, 0, 0, createButton("  1次"), GridBagConstraints.NONE, GridBagConstraints.CENTER);
+		addComponent(4, 1, 2, 1, 0, 0, createButton(" 10次"), GridBagConstraints.NONE, GridBagConstraints.CENTER);
+		addComponent(6, 1, 2, 1, 0, 0, createButton("100次"), GridBagConstraints.NONE, GridBagConstraints.CENTER);
 
-		initPanel.setOpaque(false);
-		initPanel.setVisible(true);
+	}
 
-		initPanel.setFont(new Font("宋体", Font.BOLD, 100));
-		add(initPanel);
+	private void addComponent(int gridx, int gridy, int gridwidth, int gridheight, int weightx, int weighty,
+			Component comp, int fill, int anchor) {
+		GridBagConstraints constraint = new GridBagConstraints();
+		constraint.gridx = gridx;
+		constraint.gridy = gridy;
+		constraint.gridwidth = gridwidth;
+		constraint.gridheight = gridheight;
+		constraint.weightx = weightx;
+		constraint.weighty = weighty;
+		constraint.fill = fill;
+		constraint.anchor = anchor;
+		add(comp, constraint);
 
-		JPanel timesButtonPanel = new JPanel();
+	}
 
-		JButton startAndPauseButton = new JButton("start/pause");
-		JButton onceButton = new JButton("once");
-		JButton tenTimesButton = new JButton("ten Times");
-		JButton oneHundredTimesButton = new JButton("one hundred times");
-
-		timesButtonPanel.add(startAndPauseButton);
-		timesButtonPanel.add(onceButton);
-		timesButtonPanel.add(tenTimesButton);
-		timesButtonPanel.add(oneHundredTimesButton);
-
-		tenTimesButton.setOpaque(true);
-		initPanel.setVisible(true);
-		add(timesButtonPanel);
-		
-		
-
+	private JButton createButton(String text) {
+		JButton button = new JButton(text);
+		button.setFont(new Font("微软雅黑", Font.BOLD, 15));
+		return button;
 	}
 
 	public double getScreenWidth() {
@@ -108,5 +104,10 @@ public class ControlPanel extends JPanel {
 	public void setScreenHeight() {
 		Toolkit kiToolkit = Toolkit.getDefaultToolkit();
 		this.screenHeight = kiToolkit.getScreenSize().getHeight();
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension((int) (getScreenWidth() / 4), (int) (getScreenHeight() / 2));
 	}
 }
